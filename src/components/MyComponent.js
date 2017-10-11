fc4983e54db085c2cfcdaac27ece5beb7e9f3a51
Stuart Morris <stuart@pixello.co.uk>
2017-10-11 08:28:08 +0100

    Added basic sass support

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import './MyComponent.scss'
 
-export default class App extends Component {
+export default class MyComponent extends Component {
     render() {
         return (
-            <div style={{textAlign: 'center'}}>
+            <div className='MyComponent'>
                 <h1>Hello World!</h1>
             </div>
         )
--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,41 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve('dist'),
-        filename: 'index_bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.(js|jsx)$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          }, {
+            loader: "css-loader", options: {
+              sourceMap: true
             }
+          }, {
+            loader: "sass-loader", options: {
+              sourceMap: true
+            }
+          }
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-            inject: 'body'
-        })
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
 }
